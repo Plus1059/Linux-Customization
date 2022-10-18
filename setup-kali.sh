682a49d967f2b8afb440a48b8b7eacb79db3b451
Plus1059 <112681383+Plus1059@users.noreply.github.com>
2022-10-18 00:12:16 -0700

    Update setup-kali.sh

--- setup-kali.sh
+++ setup-kali.sh
@@ -4,7 +4,7 @@ echo 'Setting Resolution'
 
 xrandr --newmode "2560x1440_60.00"  311.83  2560 2744 3024 3488  1440 1441 1444 1490  -HSync +Vsync
 xrandr --addmode Virtual-1 "2560x1440_60.00"
-xrandr --output Virtual-11 --mode "2560x1440_60.00"
+xrandr --output Virtual-1 --mode "2560x1440_60.00"
 
 echo 'Installing Nala'
 
