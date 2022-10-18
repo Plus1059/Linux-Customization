4ec1821e10cf067ab7f8c070eb50bc8060cf6698
Plus1059 <112681383+Plus1059@users.noreply.github.com>
2022-10-18 16:29:16 -0700

    Update setup-kali.sh

--- setup-kali.sh
+++ setup-kali.sh
@@ -32,6 +32,7 @@ mv kali-ascii-green.png ~/Pictures
 
 echo 'Installing Sublime-Text'
 
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo nala config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo nala update
 sudo nala install sublime-text
