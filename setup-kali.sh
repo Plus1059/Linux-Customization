36fcec222cbd8efa208109a60034c40074a855f8
Plus1059 <112681383+Plus1059@users.noreply.github.com>
2022-10-18 22:36:01 -0700

    Update setup-kali.sh

--- setup-kali.sh
+++ setup-kali.sh
@@ -29,6 +29,7 @@ mv .zshrc_Parrot_OS ~/.zshrc
 sudo mkdir ~/.config/terminator
 sudo mv config ~/.config/terminaor/config
 mv kali-ascii-green.png ~/Pictures
+wget https://store.kde.org/images/system/download.svg
 
 echo 'Installing Sublime-Text'
 
@@ -36,3 +37,6 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo nala update
 sudo nala install sublime-text -y
+
+echo 'Icon theme:
+https://store.kde.org/images/system/download.svg'
