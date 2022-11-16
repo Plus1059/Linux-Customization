158a1a0bcb7f7d0f3442d2bff66ee5744d7bfde3
Plus1059 <112681383+Plus1059@users.noreply.github.com>
2022-11-15 22:36:43 -0800

    Add Colors!

--- setup-kali.sh
+++ setup-kali.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
-echo 'Setting Resolution'
+ec="\e[0m" # End Color
+g="\e[32m" # Green
+bg="\e[1;32m" # Bold Green
+bug="\e[1;4;32m" # Bold Underlined Green
+iw="\e[3;97m" # Italic White
+m="\e[95m" # Magenta
+c="\e[96m" # Cyan
+
+echo "${bg}Setting Resolution${ec}"
 
 xrandr --newmode "2560x1440_60.00"  311.83  2560 2744 3024 3488  1440 1441 1444 1490  -HSync +Vsync
 xrandr --addmode Virtual-1 "2560x1440_60.00"
 xrandr --output Virtual-1 --mode "2560x1440_60.00"
 
-echo 'Installing Nala'
+echo "${bg}Installing Nala${ec}"
 
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 sudo apt update
 sudo apt install nala -y
 
-echo 'Updating System'
-
-sudo nala update
-sudo nala upgrade -y
-
-echo 'Setting up customizations'
+echo "${bg}Setting up customizations${ec}"
 
 sudo nala install terminator -y
 sudo nala install zsh -y
-sudo usermod --shell /bin/zsh $(whoami)
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh-autosuggestions
-sudo git git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh-syntax-highlighting
 mv .zshrc_Parrot_OS ~/.zshrc
 sudo mkdir ~/.config/terminator
 sudo mv config ~/.config/terminaor/config
 mv kali-ascii-green.png ~/Pictures
 
-echo 'Installing Sublime-Text'
+echo "${bg}Installing Sublime-Text${ec}"
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo nala update
 sudo nala install sublime-text -y
 
-echo 'Icon theme:
-https://store.kde.org/images/system/download.svg
-Material-Black-Lime-Numix-FLAT'
+echo "${bg}Updating System${ec}"
+
+sudo nala update
+sudo nala upgrade -y
+
+echo "${bug}Icon theme:${ec}
+${g}https://store.kde.org/p/1333360${ec}
+${iw}Material-Black-Lime-Numix-FLAT${ec}"
